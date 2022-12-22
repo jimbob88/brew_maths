647a82047f7ed7aa0ecd2465b2cf73d8828b45a7
James Blackburn <blackburnfjames@gmail.com>
2022-12-22 11:16:42 +0000

    Reformatted IBU calculation

--- calc/hop_bitterness.py
+++ calc/hop_bitterness.py
@@ -2,10 +2,6 @@ from calc.hop_util import utilization
 from recipe_objects.hop import HopRecipe
 
 
-# TODO: Create hop util functions first
-# https://github.com/jimbob88/wheelers-wort-works/blob/79e2e1b811e147cc0f54a7c063df02e527c74a59/beer_engine.py#L1178
-
-
 def hop_ibu(hop: HopRecipe, volume: float, boil_gravity: float) -> float:
     """
 
@@ -16,8 +12,6 @@ def hop_ibu(hop: HopRecipe, volume: float, boil_gravity: float) -> float:
     :param boil_gravity: The original gravity, calculated using the boil_volume (i.e. 1.050)
     :return: IBUs of an individual hop
     """
-    correction = 1
-    if boil_gravity > 1.050:
-        correction = 1 + (boil_gravity - 1.05) / 2
-
+    correction = 1 + (boil_gravity - 1.05) / 2 if boil_gravity > 1.050 else 1
+    
     return (hop.mass * hop.alpha * utilization(hop, boil_gravity) * 1000) / (volume * correction)
